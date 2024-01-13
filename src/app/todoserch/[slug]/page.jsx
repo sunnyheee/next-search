68eddb0f49a1500ed49b4c3311626889afaad41f
sunnyheee <fool0116@naver.com>
2024-01-13 17:35:44 +0900

    더미데이터 패치

---
+++ src/app/todoserch/[slug]/page.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const TodosSerchSlugPage = ({ params }) => {
+  const slug = params.slug;
+
+  return <div>{slug}</div>;
+};
+
+export default TodosSerchSlugPage;
---
+++ src/app/todoserch/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TodosSerchPage = () => {
+  return <div>page</div>;
+};
+
+export default TodosSerchPage;
