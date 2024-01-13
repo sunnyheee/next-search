22001e56bce903e6f961979351478f0a16367890
sunnyheee <fool0116@naver.com>
2024-01-13 16:33:07 +0900

    first commit

---
+++ src/app/layout.jsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Search",
+  description: "Search",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="ko">
+      <body>{children}</body>
+    </html>
+  );
+}
